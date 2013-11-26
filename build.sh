2a7ad999767a9b663531aa02f8d549de10413ccd
Joey Hess <joey@kitenet.net>
2013-11-26 15:29:51 -0400

    export non-compressed dmg
    The dmg is now a lot smaller, and I am going to stop producing the
    compressed version.

--- build.sh
+++ build.sh
@@ -43,7 +43,8 @@ UPGRADE_LOCATION=http://downloads.kitenet.net/git-annex/OSX/current/10.7.5_Lion/
 export UPGRADE_LOCATION
 
 make osxapp || exit 4
-cp -a tmp/git-annex.dmg.bz2 $OUTDIR_TMP
+cp -a tmp/git-annex.dmg $OUTDIR_TMP
+rm -f $OUTDIR_TMP/git-annex.dmg.bz2 # old file
 
 # put our temp files inside .git/ so ls-files doesn't see themgit ls-files --modified >.git/modified-files
 if [ -s .git/modified-files ]; then
